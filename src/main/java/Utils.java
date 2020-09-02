505129d3bba8ea9cd759d2872d3fc802c35b580b
Macarena Prado Torres <maca_pradot@hotmail.com>
2020-09-02 12:17:05 -0300

    Adding Lucene (not adding LuceneDocs)

---
+++ src/main/java/Utils.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+    private static Properties properties = new Properties();
+
+    static
+    {
+        String fileName;
+
+        try
+        {
+
+             // opcion 1
+//             fileName= "/config.txt";
+//             properties.load(Utils.class.getClass().getResourceAsStream(fileName) );
+//
+
+             // opcion 2
+             fileName= "/config.txt";
+             properties.load(Utils.class.getResourceAsStream(fileName) );
+
+
+             // opcion 3
+//             fileName= "config.txt";
+//             properties.load(Utils.class.getClassLoader().getResourceAsStream(fileName));
+             	    }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static String getDir()
+    {
+        return properties.getProperty("dir");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(Utils.getDir());
+
+    }
+
+}
